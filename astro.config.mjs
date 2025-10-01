fcaa603173eb4be689d688e97ce127cbc5421001
LoZip95 <106958217+LoZip95@users.noreply.github.com>
2025-10-02 00:13:46 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lozip95.github.io',
-    base: '/simply-static-portfolio',
+    base: '/',
     vite: { plugins: [tailwindcss()], },
 });
